4425f294235ef3b773c8035f7fde7bde7855c2a1
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-26 15:03:34 +0500

    about continud responsive

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,10 @@
 <template lang="">
     <div>
       <div class="container mx-auto md:px-0 px-2">
-        <div class='bg-[rgba(0,0,0,0.1)] w-full h-auto shadow-xl rounded-xl flex flex-col   lg:flex-row items-center justify-between py-8 px-12'>
-            <div class="text-center lg:text-left">
+        <div class='bg-[rgba(0,0,0,0.1)] w-full h-auto shadow-xl rounded-xl flex flex-col   lg:flex-row items-center  justify-between py-4 px-6 lg:py-8 lg:pr-10 lg:pl-20'>
+            <div class="text-center lg:text-left mb-2 md:mb-3 lg:mb-0">
                 <h4 class="text-[20px] md:text-[26px] lg:text-[36px] font-[Jost]  mb-1 lg:mb-3 text-[#0a6272]">IMRON PLAST</h4>
-                <p class="text-[12px] md:text-[14px] text-gray-700 font-medium max-w-[600px] text-center lg:text-justify mb-2 md:mb-3 lg:mb-0">Компания IMRON Plast начала свою деятельность во второй половине 2008 года. На протяжении многих лет он предоставляет большому количеству клиентов качественную продукцию, специализированный сервис и качественную и доступную продукцию. По этим причинам мы много лет тестировали и совершенствовали свою продукцию и расширялись в больших масштабах. Наш слоган: <br>
+                <p class="text-[12px] md:text-[14px] text-gray-700 font-medium max-w-[600px] text-center lg:text-justify ">Компания IMRON Plast начала свою деятельность во второй половине 2008 года. На протяжении многих лет он предоставляет большому количеству клиентов качественную продукцию, специализированный сервис и качественную и доступную продукцию. По этим причинам мы много лет тестировали и совершенствовали свою продукцию и расширялись в больших масштабах. Наш слоган: <br>
             <span class="text-[14px] md:text-[16px] lg:text-[20px] font-[Jost] font-bold">Выгода от честности всегда превышает выгоду.</span></p>
             </div>
             <img src="@/assets/site-logo/site-logo-6.png" alt="about image" class="w-[200px] h-[120px] md:w-[350px] md:h-[200px] lg:w-[500] lg:h-[300px] object-cover">
