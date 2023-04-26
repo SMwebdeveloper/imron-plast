a890dca43f68678d504e8ae9e0dada79dccd3559
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-26 15:14:05 +0500

    container padding

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template lang="">
     <div id='#about'>
-      <div class="container mx-auto md:px-0 px-2">
+      <div class="container mx-auto md:px-0 px-3">
         <div class='bg-[rgba(0,0,0,0.1)] w-full h-auto shadow-xl rounded-xl flex flex-col   lg:flex-row items-center  justify-between py-4 px-6 lg:py-8 lg:pr-10 lg:pl-20'>
             <div class="text-center lg:text-left mb-2 md:mb-3 lg:mb-0">
                 <h4 class="text-[20px] md:text-[26px] lg:text-[36px] font-[Jost]  mb-1 lg:mb-3 text-[#0a6272]">IMRON PLAST</h4>
