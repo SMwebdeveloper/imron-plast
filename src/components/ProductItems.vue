298b1216ac2cc0439785e1f54c67b3a5324a98e4
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-27 10:26:26 +0500

    products width

--- src/components/ProductItems.vue
+++ src/components/ProductItems.vue
@@ -1,5 +1,5 @@
 <template lang="">
-  <div class="text-center bg-[white] flex flex-col mx-auto items-center shadow-md rounded-md w-[200px] h-[220px] md:w-[250px] md:h-[260px]  lg:h-[300px]">
+  <div class="text-center bg-[white] flex flex-col mx-auto items-center shadow-md rounded-md w-[180px] h-[220px] md:w-[250px] md:h-[260px]  lg:h-[300px]">
     <img :src="getPic(product.img)" :alt="product.title" class="max-w-[150px] h-[70%] object-contain mb-3">
     <h5 class="text-[12px] md:text-[14px] lg:text-[16px] max-w-[250px]">{{product.title}}</h5>
   </div>
