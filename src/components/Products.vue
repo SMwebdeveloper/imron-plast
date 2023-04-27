dfa8645fd79e4c2192aa7159c176bba270c5c100
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-27 15:01:55 +0500

    prodcts carousel

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,12 +3,12 @@
       <div class="container mx-auto md:px-0 px-3">
         <div class="text-center">
             <h2 class="text-[20px] md:text-[28px] lg:text-[36px] text-[#4f83a7] font-semibold mb-3">Каталог</h2>
-            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-5 text-center">
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-5 text-center mb-16">
                 <ProductItems v-for="product in products" :key="product.title" :product="product"/>
             </div>
            <div>
-             <h3>пакеты с вашим логотипом</h3>
              <!-- carousel -->
+             <ProductCarousel/>
            </div>
         </div>
       </div>
@@ -16,19 +16,18 @@
 </template>
 <script>
 import ProductItems from './ProductItems.vue';
+import ProductCarousel from './ProductCarousel.vue';
 import data from '@/data.json'
 export default {
   name: 'ProductComponent',
   components: {
-    ProductItems
+    ProductItems,
+    ProductCarousel
   },
   data () {
     return {
         products: data.products
     }
-  },
-  mounted () {
-    console.log(this.products)
   }
 }
 </script>
