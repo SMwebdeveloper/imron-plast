34a7508b8590d31dff555f62c91974e11b57bce0
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-28 10:19:53 +0500

    Unnecessary elements are deleted

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
       <div class="container mx-auto md:px-0 px-3">
         <div class="w-full flex items-center justify-between px-10">
             <img src="@/assets/site-logo/site-logo.svg" alt="site logo" class="w-[120px] lg:w-[150px] h-[50px] object-cover ml-[-15px]">
-            <h6 class="p-2 bg-blue-500 rounded-md text-[11px] md:text-[16px] lg:text-[18px] font-medium text-gray-800">+998-90-390-64-75</h6>
+            <p class="text-[10px] md:text-[14px] lg:text-[16px] font-semibold text-[#327b82] text-center">Выгода от честности всегда превышает выгоду.</p>
         </div>
       </div>
     </div>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,22 @@
 <template>
     <div class="bg-white shadow-md py-2 mb-5 fixed top-0 left-0 w-screen z-10">
-       <div class="container mx-auto md:px-0 px-3">
+       <div class="container mx-auto md:px-0 px-3 pr-6">
         <div class="flex w-full items-center justify-between flex-col lg:flex-row">
             <div class="flex items-center justify-between lg:w-auto md:w-full sm:w-full w-full">
                 <img src="@/assets/site-logo/site-logo.svg" alt="site-logo" class="w-[120px] lg:w-[150px] h-[50px] object-cover ml-[-15px]">
                 <div class="lg:hidden block">
-                   <button class="text-3xl cursor-pointer mr-3" @click="toggleBtn">
-                    <i class="fa-solid fa-bars" v-if="!menu"></i>
-                    <i class="fa-solid fa-xmark" v-else></i>
+                   <button class="text-3xl cursor-pointer" @click="toggleBtn">
+                      <ion-icon name="menu-outline" v-if="!menu"></ion-icon>
+                      <ion-icon name="close-outline" v-else></ion-icon>
                    </button>
                 </div>
             </div>
-            
-            <div class="lg:flex" :class="`${menu ? 'block pb-3 w-full' : 'hidden'}`">
-                <ul class="flex flex-col items-start lg:flex-row lg:items-center mr-0 lg:mr-5">
-                    <li class=" text-[16px] inline-block font-medium text-[#072430] mr-7 last:mr-0 hover:text-[#999696] duration-75 capitalize" v-for="item in navigation" :key="item.title">
-                      <a :href="item.href">{{ item.title }}</a>
-                    </li>
-                </ul>
-                <h6 class="p-2 inline-block bg-blue-500 rounded-md text-[14px]md:text-[16px] lg:text-[18px] font-medium text-gray-800">+998-90-390-64-75</h6>
-            </div>
+
+            <ul class="lg:flex" :class="`${menu ? 'block pb-3 w-full' : 'hidden'}`">
+                <li class=" text-[16px] font-medium text-[#072430] mr-7 last:mr-0 hover:text-[#999696] duration-75 capitalize" v-for="item in navigation" :key="item.title">
+                  <a :href="item.href">{{ item.title }}</a>
+                </li>
+            </ul>
         </div>
        </div>
     </div>
