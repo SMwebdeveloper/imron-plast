a797e8d68d15935d1d7c502423e81fe6a6fea8e6
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-30 12:35:31 +0500

    maps

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -12,7 +12,7 @@
       >
         <LocationItem />
         <!-- maps -->
-        <div class="border-solid border-gray-500 border-[4px] rounded-md">
+        <div class="ml-[220px] border-solid border-gray-500 border-[4px] rounded-md">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3039.308599585619!2d71.75495990961402!3d40.3798525713265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bb85c72cf64211%3A0x53ba3e91d7bf0cf4!2sIMRON%20Plast!5e0!3m2!1suz!2s!4v1682753012966!5m2!1suz!2s"
             width="600"
