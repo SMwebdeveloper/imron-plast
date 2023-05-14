6ef9fae01b6df2c6e6fa7309653023931a717742
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-14 16:27:25 +0500

    about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,13 +1,9 @@
 <template lang="">
-    <div id='about'>
-      <div class="container mx-auto md:px-0 px-3">
-        <div class='bg-[rgba(0,0,0,0.1)] w-full h-auto shadow-lg rounded-xl flex flex-col   lg:flex-row items-center  justify-between py-4 px-6 lg:py-8 lg:pr-10 lg:pl-20'>
-            <div class="text-center lg:text-left mb-2 md:mb-3 lg:mb-0">
-                <h4 class="text-[20px] md:text-[26px] lg:text-[36px] font-[Jost]  mb-1 lg:mb-3 text-[#0a6272]">IMRON PLAST</h4>
-                <p class="text-[12px] md:text-[14px] text-gray-700 font-medium max-w-[650px] text-center lg:text-justify ">Компания IMRON Plast начала свою деятельность во второй половине 2008 года. На протяжении многих лет он предоставляет большому количеству клиентов качественную продукцию, специализированный сервис, качественную и доступную продукцию. По этим причинам мы много лет тестировали и совершенствовали свою продукцию и расширялись в больших масштабах. Наш слоган: <br>
-            <span class="text-[14px] md:text-[16px] lg:text-[20px] font-[Jost] font-bold">Выгода от честности всегда превышает выгоду.</span></p>
-            </div>
-            <img src="@/assets/images/site-logo-6.png" alt="about image" class="w-[200px] h-[120px] md:w-[350px] md:h-[200px] lg:w-[500] lg:h-[300px] object-cover">
+    <div id='about' class="bg-image bg-cover bg-center backdrop-blur-sm h-[50vh] md:h-[80vh] lg:h-[100vh] w-screen">
+      <div class="container mx-auto">
+        <div class="flex flex-col items-center justify-center w-screen h-screen">
+          <h2 class="text-[32px] md:text-[70px] mb-5 font-extrabold uppercase gradient-text shadow-md">Imron Plast</h2>
+          <h5 class="text-[28px] text-">Выгода от честности всегда превышает выгоду.</h5>
         </div>
       </div>
     </div>
