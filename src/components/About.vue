d4fa59be94794e36c8022abd115ad3365e6d2349
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-15 22:06:36 +0500

    about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,10 @@
 <template lang="">
     <div id='about' class="bg-image bg-cover bg-center backdrop-blur-sm h-[50vh] md:h-[80vh] lg:h-[100vh] w-screen">
       <div class="container mx-auto">
-        <div class="flex flex-col items-center justify-center w-screen h-screen">
-          <h2 class="text-[32px] md:text-[70px] mb-5 font-extrabold uppercase gradient-text shadow-md">Imron Plast</h2>
-          <h5 class="text-[28px] text-">Выгода от честности всегда превышает выгоду.</h5>
+        <div class="flex flex-col items-center justify-center w-screen h-[50vh] md:h-[80vh] lg:h-[100vh]">
+          <h2 class="text-[32px] md:text-[70px] lg:text-[100px] mb-5 font-extrabold uppercase gradient-text shadow-md">Imron Plast</h2>
+          <h5 class="text-[28px] text-white">Выгода от честности всегда превышает выгоду.</h5>
+          <button>More</button>
         </div>
       </div>
     </div>
