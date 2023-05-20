8bfaba1e771028f95290bd93f8c85bdbf71d4397
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-20 23:22:36 +0500

    about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,10 @@
     <div id='about' class="bg-imron-img bg-center bg-cover bg-fixed h-[50vh] md:h-[80vh] lg:h-[100vh] w-full">
       <div class="container mx-auto px-2">
         <div class="flex flex-col items-center justify-center h-[50vh] md:h-[80vh] lg:h-[100vh]">
-          <h2 class="text-[32px] md:text-[70px] text-gray-800 lg:text-[100px] mb-3 font-extrabold uppercase">Imron Plast</h2>
+          <div class="text-[32px] md:text-[70px] text-gray-800 lg:text-[100px] mb-3 font-extrabold uppercase">
+            <div>Imron Plast</div>
+            
+          </div>
           <h5 class="text-[16px] md:text-[22px] lg:text-[28px] text-gray-900 uppercase text-center">Выгода от честности всегда превышает выгоду.</h5>
           <button class="p-5 border-[#ccc] border-solid border-[1px]">More</button>
         </div>
