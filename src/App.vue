780f7d45fc41bf31624a1a7ed68cb89b94c9fd43
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-23 10:39:58 +0500

    about section

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="bg-gray-50 h-full bg-gradient-to-r from-gray-200 to-[#ccc] overflow-x-hidden">
+  <div class="bg-gray-50 h-full bg-gradient-to-r from-[#f5f5f5] to-[#f8f8f8] overflow-x-hidden">
     <Navbar/>
+    <Hero/>
     <About/>
     <Products/>
     <Partners/>
@@ -12,6 +13,7 @@
 
 <script>
 import Navbar from './components/Navbar';
+import Hero from './components/Hero.vue';
 import About from './components/About.vue';
 import Products from './components/Products.vue';
 import Partners from './components/Partners.vue';
@@ -23,6 +25,7 @@ export default {
   name: 'App',
   components: {
     Navbar,
+    Hero,
     About,
     Products,
     Partners,
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,37 +1,21 @@
 <template lang="">
-  <div id="about" class="bg-image h-[50vh] md:h-[80vh] lg:h-[100vh] w-full">
-    <div class="container mx-auto px-2">
-      <div
-        class="flex flex-col items-center pt-[70px] lg:pt-[150px] h-[50vh] md:h-[80vh] lg:h-[100vh]"
-      >
-        <h2
-          data-aos="fade-up"
-          data-aos-easing="ease-in-back"
-          data-aos-duration="2000"
-          class="text-[40px] text-[#fff] mb-1 md:text-[70px] lg:text-[100px] font-extrabold uppercase flex items-center"
-        >
-          Imron
-          <span
-            class="border-l-4 lg:border-l-8 border-[#0a3549] text-[#0a3549] text-[25px] lg:text-[60px] flex items-start p-0 h-[30px] lg:h-[70px] ml-2 lg:ml-4"
-            >Plast</span
-          >
-        </h2>
-        <h6
-          data-aos="zoom-in-right"
-          data-aos-anchor="#example-anchor"
-          data-aos-duration="2000"
-          class="text-[16px] md:text-[24px] lg:text-[28px] font-bold text-[#ccc] uppercase text-center tracking-wider max-w-[650px]"
-        >
-          Выгода от честности всегда превышает выгоду.
-        </h6>
-        <!-- <button class="p-5 border-[#ccc] border-solid border-[1px]">More</button> -->
-      </div>
+    <div id="about">
+        <div class="container mx-auto px-2">
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 <script>
 export default {
-  name: "AboutComponent",
-};
+    name:'AboutComponent'
+    
+}
 </script>
-<style lang=""></style>
+<style lang="">
+    
+</style>
+
