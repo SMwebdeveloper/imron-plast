8e82347e9cf3400856f864acd578baefe9ce9306
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-25 22:56:53 +0500

    vue typer and app

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <template>
   <div class="overflow-x-hidden">
     <Navbar/>
-    <Hero/>
-    <About/>
-    <Products/>
-    <Partners/>
-    <Delivery/>
-    <Location/>
-    <Footer/>
+     <Hero/>
+   <!-- <About/> -->
+    <!-- <Products/> -->
+    <!-- <Partners/> -->
+    <!-- <Delivery/> -->
+    <!-- <Location/> -->
+    <!-- <Footer/> -->
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar';
 import Hero from './components/Hero.vue';
-import About from './components/About.vue';
-import Products from './components/Products.vue';
-import Partners from './components/Partners.vue';
-import Delivery from './components/Delivery.vue';
-import Location from './components/Location.vue';
-import Footer from './components/Footer.vue';
+// import About from './components/About.vue';
+// import Products from './components/Products.vue';
+// import Partners from './components/Partners.vue';
+// import Delivery from './components/Delivery.vue';
+// import Location from './components/Location.vue';
+// import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Hero,
-    About,
-    Products,
-    Partners,
-    Delivery,
-    Location,
-    Footer
+    Hero
+    // About,
+    // Products,
+    // Partners,
+    // Delivery,
+    // Location,
+    // Footer
   }
 }
 </script>
