3ebfc1e5cd7830f58ba469a42695c91dbf656b47
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-25 22:57:45 +0500

    product item

--- src/components/ProductItems.vue
+++ src/components/ProductItems.vue
@@ -1,7 +1,7 @@
 <template lang="">
-  <div class="text-center bg-[white] flex flex-col mx-auto items-center shadow-md rounded-md w-[150px] h-[180px] md:w-[250px] md:h-[260px] lg:w-[300px] lg:h-[300px] pb-3 px-2">
+  <div class="text-center bg-[#e8e8e8] flex flex-col items-center shadow-md rounded-md w-[150px] h-[180px] md:w-[250px] md:h-[260px] lg:w-[295px] m-1 lg:h-[300px] pb-3 px-2">
     <img :src="getPic(product.img)" :alt="product.title" class="w-[100px] md:w-[220px] h-[70%] object-contain mb-5">
-    <h5 class="text-[12px] md:text-[14px] text-[#318888] lg:text-[16px] max-w-[140px] md:max-w-[200px]">{{product.title}}</h5>
+    <h5 class="text-[12px] md:text-[14px] text-[#030505] lg:text-[16px] max-w-[140px] md:max-w-[200px] text-left">{{product.title}}</h5>
   </div>
 </template>
 <script>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,9 +1,9 @@
 <template lang="">
     <div id="products" class="mb-5 lg:mb-16 pt-16">
-      <div class="container mx-auto md:px-0 px-3">
-        <div class="text-center">
+      <div class="container mx-auto px-3">
+        <div class="">
             <h2 class="text-[20px] md:text-[28px] lg:text-[36px] text-[#4f83a7] font-semibold mb-3">Каталог</h2>
-            <div class="grid items-center grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-5 mb-8">
+            <div class="flex flex-wrap justify-center items-center  mb-8">
                 <ProductItems v-for="product in products" :key="product.title" :product="product"/>
             </div>
             <!-- <button  class="mx-auto py-3 px-8 text-[18px] text-white bg-[#1a5c6a] hover:bg-[#249f99] duration-300 rounded-xl mb-10">Более</button> -->
