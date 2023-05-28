c28c110ad7304a70a895971d12d2b879a41aedd5
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-28 22:05:16 +0500

    choose

---
+++ src/components/Choose.vue
@@ -0,0 +1,46 @@
+<template lang="">
+  <div class="bg-white py-5">
+    <div class="container mx-auto px-3 md:px-0 text-center">
+        <h2 class=" text-[22px] sm:text-[24px] md:text-[30px] lg:text-[34px] text-blue-400 mb-2 ">Why choose us</h2>
+        <p class="w-[250px] md:w-[350px] lg:w-[400px] text-sm md:text-md lg:text-base mx-auto">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, quos.</p>
+        <div class="flex flex-wrap gap-2 md:gap-3 lg:gap-5 items-center justify-between w-full">
+          <div class="w-full lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0">
+            <i>Icon</i>
+            <h4 class="text-base md:text-lg font-medium text-gray-500">Lorem, ipsum dolor.</h4>
+            <p class="text-xs sm:text-sm md:text-md text-gray-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
+          </div>
+          <div class="w-full lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0">
+            <i>Icon</i>
+            <h4 class="text-base md:text-lg font-medium text-gray-500">Lorem, ipsum dolor.</h4>
+            <p class="text-xs sm:text-sm md:text-md text-gray-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
+          </div>
+          <div class="w-full lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0">
+            <i>Icon</i>
+            <h4 class="text-base md:text-lg font-medium text-gray-500">Lorem, ipsum dolor.</h4>
+            <p class="text-xs sm:text-sm md:text-md text-gray-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
+          </div>
+          <div class="w-full lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0">
+            <i>Icon</i>
+            <h4 class="text-base md:text-lg font-medium text-gray-500">Lorem, ipsum dolor.</h4>
+            <p class="text-xs sm:text-sm md:text-md text-gray-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
+          </div>
+          <!-- <div>
+            <i>Icon</i>
+            <h4>Lorem, ipsum dolor.</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
+          </div>
+          <div>
+            <i>Icon</i>
+            <h4>Lorem, ipsum dolor.</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
+          </div> -->
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ChooseComponent",
+};
+</script>
+<style lang=""></style>
