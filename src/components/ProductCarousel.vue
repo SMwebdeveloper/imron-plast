53c29cfa9cd8563592001883ef7c71ff0e6af832
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-28 22:06:05 +0500

    product carousel

--- src/components/ProductCarousel.vue
+++ src/components/ProductCarousel.vue
@@ -1,20 +1,38 @@
 <template lang="">
-  <div class="flex flex-col lg:flex-row items-center lg:justify-between lg:h-[380px] bg-white mb-9 rounded-lg shadow-lg py-5 px-6 lg:px-14">
-    <div class="lg:text-left">
-      <h3 class="text-[18px] md:text-[24px] lg:text-[28px] font-semibold text-gray-500 mb-1 md:mb-2 lg:mb-3">Пакеты с вашим логотипом</h3>
-      <h6 class="text-[12px] md:text-[14px] lg:text-[18px] lg:w-[500px] font-bold text-gray-400">Наши продукты с вашим логотипом для продвижения вашего бизнеса</h6>
-    </div>
-    <div class='max-w-[550px]'>
-      <carousel
-        :perPage="2"
-        paginationEnable="true"
-        paginationColor="#b3b3b3"
-        paginationActiveColor="#494ce8"
+  <div class="bg-white mb-9 py-5 px-6 lg:px-14">
+    <div class="container mx-auto px-3 md:px-0">
+      <div
+        class="flex flex-col items-center justify-center  w-full"
       >
-        <slide v-for="(item, index) in slide" :key="index">
-          <img :src="getPic(item.img)" alt="bag logo" class="w-[270px] sm:w-[400px] md:w-[450px] h-[250px] lg:w-[500px] object-contain mb-0 mx-auto my-auto" />
-        </slide>
-      </carousel>
+        <div class="lg:text-center">
+          <h3
+            class="text-[18px] md:text-[24px] lg:text-[38px] font-semibold text-blue-500 mb-1 md:mb-2 lg:mb-3"
+          >
+            Пакеты с вашим логотипом
+          </h3>
+          <h6
+            class="text-[12px] md:text-[14px] lg:text-[18px] lg:w-[500px] font-bold text-gray-400"
+          >
+            Наши продукты с вашим логотипом для продвижения вашего бизнеса
+          </h6>
+        </div>
+        <div class="max-w-[550px]">
+          <carousel
+            :perPage="2"
+            paginationEnable="true"
+            paginationColor="#b3b3b3"
+            paginationActiveColor="#494ce8"
+          >
+            <slide v-for="(item, index) in slide" :key="index">
+              <img
+                :src="getPic(item.img)"
+                alt="bag logo"
+                class="w-[270px] sm:w-[400px] md:w-[450px] h-[250px] lg:w-[500px] object-contain mb-0 mx-auto my-auto"
+              />
+            </slide>
+          </carousel>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -55,35 +73,35 @@ export default {
         },
         {
           id: "9",
-          img: "asia-png"
+          img: "asia-png",
         },
         {
           id: "10",
-          img: "istiqlol-png"
+          img: "istiqlol-png",
         },
         {
           id: "125",
-          img: "asmo-png"
+          img: "asmo-png",
         },
         {
           id: "13",
-          img: "davr-png"
+          img: "davr-png",
         },
         {
           id: "11",
-          img: "sarbon-png"
+          img: "sarbon-png",
         },
         {
           id: "12",
-          img: "gilam-png"
+          img: "gilam-png",
         },
         {
           id: "14",
-          img: "rash-milk-png"
+          img: "rash-milk-png",
         },
         {
           id: "7",
-          img: "suv-png"
+          img: "suv-png",
         },
         {
           id: "18",
@@ -91,20 +109,20 @@ export default {
         },
         {
           id: "8",
-          img: "dinar-png"
+          img: "dinar-png",
         },
       ],
     };
   },
   methods: {
     getPic(img) {
-        return require(`@/assets/images/${img}.png`);
-    }
-  }
+      return require(`@/assets/images/${img}.png`);
+    },
+  },
 };
 </script>
 <style lang="css">
-.VueCarousel-dot-container{
-    margin: 0;
+.VueCarousel-dot-container {
+  margin: 0;
 }
 </style>
