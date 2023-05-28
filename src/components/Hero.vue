d69545818aba5a7734366dcff5d2efcf1a0e7ef7
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-28 22:05:25 +0500

    hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,8 @@
 <template lang="">
-  <div class="bg-white">
+  <div class="bg-white h-[40vh] sm:h-[70v] lg:h-[100vh]">
     <div class="container mx-auto px-3 md:px-0">
-      <div class="flex flex-col items-center pt-[60px] sm:pt-[60px] md:pt-[120px] h-[40vh] sm:h-[50vh] md:h-[70vh] lg:h-[100vh]">
-        <div class="text-[28px] md:text-[48px] lg:text-[65px] 2xl:text-[72px] text-center lg:w-[70%] text-blue-400 mx-auto font-bold mb-[40px] lg:mb-[120px]">
+      <div class="flex flex-col items-center pt-[60px] sm:pt-[60px] md:pt-[120px] ">
+        <div class="text-[28px] md:text-[48px] lg:text-[65px] 2xl:text-[72px] text-center lg:w-[70%] text-blue-400 mx-auto font-bold mb-[40px] lg:mb-[150px]">
           <h2 class="m-0">Имрон Пласт:</h2>
           <VueTyper
             :text="[
@@ -22,9 +22,8 @@
             caret-animation="solid"
           ></VueTyper>
         </div>
-
         <div class="text-center">
-          <p class="text-[14px] md:text-[16px] lg:text-[20px] text-[#060606] w-[350px] md:w-[450px] lg:w-[650px] font-normal mb-2">У нас есть цель: помочь нашим партнерам быть более успешными. Это то, над чем мы эффективно работаем каждый день.</p>
+          <p class="text-md sm:text-lg md:text-xl lg:text-2xl text-blue-400 w-[350px] md:w-[450px] lg:w-[650px] font-normal mb-4">У нас есть цель: помочь нашим партнерам быть более успешными.</p>
           <button class="border-blue-400 text-blue-400 fonst-medium border-solid border-[1px] py-2 px-6 rounded-lg duration-300 hover:border-white hover:bg-blue-400 hover:text-white">Вперед</button>
         </div>
       </div>
@@ -37,17 +36,7 @@ export default {
   name: "HeroComponent",
   components: {
     VueTyper,
-  },
-  data() {
-    return {
-      lab: 2,
-    };
-  },
-  watch: {
-    lab: function () {
-      console.log(3);
-    },
-  },
+  }
 };
 </script>
 <style lang=""></style>
