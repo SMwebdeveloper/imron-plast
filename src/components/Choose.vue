5a9e44995068246608642a859c21f8a748a6feea
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-31 23:38:07 +0500

    choose

--- src/components/Choose.vue
+++ src/components/Choose.vue
@@ -1,40 +1,72 @@
 <template lang="">
   <div class="bg-white py-5">
     <div class="container mx-auto px-3 md:px-0 text-center">
-        <h2 class=" text-[22px] sm:text-[24px] md:text-[30px] lg:text-[34px] text-blue-400 mb-2 ">Why choose us</h2>
-        <p class="w-[250px] md:w-[350px] lg:w-[400px] text-sm md:text-md lg:text-base mx-auto">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, quos.</p>
-        <div class="flex flex-wrap gap-2 md:gap-3 lg:gap-5 items-center justify-between w-full">
-          <div class="w-full lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0">
-            <i>Icon</i>
-            <h4 class="text-base md:text-lg font-medium text-gray-500">Lorem, ipsum dolor.</h4>
-            <p class="text-xs sm:text-sm md:text-md text-gray-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
-          </div>
-          <div class="w-full lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0">
-            <i>Icon</i>
-            <h4 class="text-base md:text-lg font-medium text-gray-500">Lorem, ipsum dolor.</h4>
-            <p class="text-xs sm:text-sm md:text-md text-gray-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
-          </div>
-          <div class="w-full lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0">
-            <i>Icon</i>
-            <h4 class="text-base md:text-lg font-medium text-gray-500">Lorem, ipsum dolor.</h4>
-            <p class="text-xs sm:text-sm md:text-md text-gray-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
-          </div>
-          <div class="w-full lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0">
-            <i>Icon</i>
-            <h4 class="text-base md:text-lg font-medium text-gray-500">Lorem, ipsum dolor.</h4>
-            <p class="text-xs sm:text-sm md:text-md text-gray-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
-          </div>
-          <!-- <div>
-            <i>Icon</i>
-            <h4>Lorem, ipsum dolor.</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
-          </div>
-          <div>
-            <i>Icon</i>
-            <h4>Lorem, ipsum dolor.</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sequi tempora rem ab soluta est fugiat dolore incidunt illo cupiditate!</p>
-          </div> -->
+      <h2
+        class="text-[22px] sm:text-[24px] md:text-[30px] lg:text-[34px] text-blue-400 mb-2"
+      >
+        Почему выбрали нас
+      </h2>
+      <p
+        class="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] text-sm md:text-base lg:text-lg text-slate-500 mx-auto mb-6"
+      >
+        Спасибо, что считаете нас партнером. Мы верим, что наш опыт, наши навыки
+        и преданность делу отличает нас и делает нас идеальным выбором для
+        вашего бизнеса.
+      </p>
+      <div
+        class="flex flex-wrap gap-2 md:gap-3 lg:gap-5 items-center justify-between w-full"
+      >
+        <div
+          class="w-full h-[135px] lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0"
+        >
+          <i class="fa-solid fa-briefcase bg-sky-300 text-gray-50 text-xs md:text-base  px-[6px] md:px-3 py-1 md:py-2 rounded-full"></i>
+          <h4 class="text-base md:text-lg font-medium text-sky-600">
+            Специалист
+          </h4>
+          <p class="text-xs sm:text-sm font-medium md:text-md text-slate-700">
+            Наша компания работает с 2008 года. До сих пор с нами работали
+            только специалисты, и это стало традицией нашей компании.
+          </p>
         </div>
+        <div
+          class="w-full h-[150px] md:h-[135px] lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0"
+        >
+          <i class="fa-solid fa-truck-ramp-box bg-sky-300 text-gray-50 text-[11px] md:text-sm  px-[6px] md:px-[10px] py-[7px] md:py-2 rounded-full"></i>
+          <h4 class="text-base md:text-lg font-medium text-sky-600">
+            Доставка
+          </h4>
+          <p class="text-xs sm:text-sm font-medium md:text-md text-slate-700">
+            Хотите заказать наш товар? Но вы из другой провинции или соседней
+            страны? Также у нас есть служба доставки. Мы доставляем нашу
+            продукцию куда угодно.
+          </p>
+        </div>
+        <div
+          class="w-full h-[135px] lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0"
+        >
+          <i class="fa-solid fa-message bg-sky-300 text-gray-50 text-[10px] md:text-base  px-[7px] md:px-3 py-2 md:py-2 rounded-full"></i>
+          <h4 class="text-base md:text-lg font-medium text-sky-600">
+            Коммуникация
+          </h4>
+          <p class="text-xs sm:text-sm font-medium md:text-md text-slate-700">
+            Мы считаем, что четкая и постоянная коммуникация имеет решающее
+            значение для успеха любого проекта.
+          </p>
+        </div>
+        <div
+          class="w-full h-[150px] md:h-[135px] lg:w-[49%] text-left bg-gray-200 rounded-md p-3 shadow-xl mb-4 lg:mb-0"
+        >
+          <i class="fa-solid fa-square-check bg-sky-300 text-gray-50 text-xs md:text-base  px-[6px] md:px-3 py-1 md:py-2 rounded-full"></i>
+          <h4 class="text-base md:text-lg font-medium text-sky-600">
+            Внимание к детали
+          </h4>
+          <p class="text-xs sm:text-sm font-medium md:text-md text-slate-700 text-justify">
+            Мы гордимся своей работой и тем, что конечный продукт имеет
+            высочайшее качество. Мы уделяем пристальное внимание каждой детали,
+            чтобы обеспечить
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
