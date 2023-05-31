0a0bd76e63980cf6e0b2da5d362992df9471b345
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-31 23:37:36 +0500

    hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,8 @@
 <template lang="">
-  <div class="h-[45vh] sm:h-[70v] lg:h-[100vh] bg-imron-img bg-no-repeat bg-cover bg-center">
-    <div class="container mx-auto px-3 md:px-0">
-      <div class="flex flex-col items-center pt-[60px] sm:pt-[60px] md:pt-[120px] ">
-        <div class="text-[26px] md:text-[48px] lg:text-[65px] 2xl:text-[72px] text-center lg:w-[70%] text-blue-400 mx-auto font-bold mb-[40px] lg:mb-[150px]">
+  <div class="h-[40vh] sm:h-[70v] lg:h-[100vh] bg-imron-img bg-no-repeat bg-cover bg-top">
+    <div class="container mx-auto px-3 lg:px-0">
+      <div class="flex flex-col items-center pt-[60px] sm:pt-[60px] md:pt-[120px] 2xl:pt-[200px]">
+        <div class="text-[26px] md:text-[48px] lg:text-[65px] 2xl:text-[80px] text-center lg:w-[70%] text-blue-400 mx-auto font-bold mb-[40px] lg:mb-[150px] 2xl:mb-[250px]">
           <h2 class="m-0">Имрон Пласт:</h2>
           <VueTyper
             :text="[
@@ -23,7 +23,7 @@
           ></VueTyper>
         </div>
         <div class="text-center">
-          <p class="text-md sm:text-lg md:text-xl lg:text-2xl text-blue-400 w-[320px] md:w-[450px] lg:w-[650px] font-normal mb-4">У нас есть цель: помочь нашим партнерам быть более успешными.</p>
+          <p class="text-md sm:text-lg md:text-xl lg:text-2xl 2xl:text-[26px] text-blue-400 w-[320px] md:w-[450px] lg:w-[650px] font-normal mb-4 2xl:mb-8">У нас есть цель: помочь нашим партнерам быть более успешными.</p>
           <button class="border-blue-400 text-blue-400 fonst-medium border-solid border-[1px] py-2 px-6 rounded-lg duration-300 hover:border-white hover:bg-blue-400 hover:text-white">Вперед</button>
         </div>
       </div>
